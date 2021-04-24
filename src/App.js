719952fc3243a171e3ca8d293a574c724273939a
Md. Altaf Hoshain <altaf@squaregroup.com>
2021-04-24 15:32:03 +0600

    Token based login done

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React, { useState } from 'react';
+
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import TopNav from './components/common/TopNav';
@@ -8,19 +10,27 @@ import Category from "./components/category/Category";
 import Product from "./components/products/Product";
 
 import Login from "./components/auth/Login";
+import UseToken from './components/auth/UseToken';
 
 function App() {
+
+  const { token, setToken } = UseToken();
+
+  if (!token) {
+    return <Login setToken={setToken} />
+  }
+
   return (
     <Router>
       <div>
-       <TopNav />
+        <TopNav />
         <Layout>
           <Switch>
 
             <Route exact path="/">
               <Home />
             </Route>
-            
+
             <Route path="/category">
               <Category />
             </Route>
@@ -28,7 +38,7 @@ function App() {
             <Route path="/product">
               <Product />
             </Route>
-          
+
           </Switch>
         </Layout>
         <Footer />
--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,17 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import { Card, Form, Button } from 'react-bootstrap';
 
 import "./Login.css";
 
-const Login = () => {
+
+async function loginUser(credentials) {
+   
+    //change this later as post request
+   /*
+    return fetch('http://localhost:3030/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(credentials)
+    })
+        .then(data => data.json())
+
+    */
+
+    return fetch('https://jsonblob.com/api/593d571f-a4da-11eb-acf4-9b6752717560', {
+        method: 'GET'
+    })
+        .then(data => data.json())
+}
+
+
+const Login = ({ setToken }) => {
+
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        const token = await loginUser({
+            email,
+            password
+        });
+        setToken(token);
+    }
+
     return (
         <Card className="card-top">
             <Card.Header className="text-center">Login to your account</Card.Header>
             <Card.Body>
-                <Form>
+                <Form onSubmit={handleSubmit}>
                     <Form.Group controlId="formBasicEmail">
                         <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Control type="email" placeholder="Enter email" onChange={e => setEmail(e.target.value)} />
                         <Form.Text className="text-muted">
                             We'll never share your email with anyone else.
                 </Form.Text>
@@ -19,10 +56,10 @@ const Login = () => {
 
                     <Form.Group controlId="formBasicPassword">
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
+                        <Form.Control type="password" placeholder="Password" onChange={e => setPassword(e.target.value)} />
                     </Form.Group>
                     <Form.Group controlId="formBasicCheckbox">
-                        <Form.Check type="checkbox" label="Check me out" />
+                        <Form.Check type="checkbox" label="Remember me" />
                     </Form.Group>
                     <Button variant="primary" type="submit">
                         Submit
