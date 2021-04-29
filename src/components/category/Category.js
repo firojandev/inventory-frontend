67cbbc7b08f1ac00a97a908dde0902622b655363
Md. Altaf Hoshain <altaf@squaregroup.com>
2021-04-29 14:20:02 +0600

    Categpry tabel modified

--- src/components/category/Category.js
+++ src/components/category/Category.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 
 import Table from 'react-bootstrap/Table'
+import Button from 'react-bootstrap/Button'
 
 import axios from 'axios';
 
@@ -19,7 +20,20 @@ const Category = () => {
     return (
         <div>
 
-            <h3>Category list</h3>
+           
+           <div className="mb-2 pb-4 pt-4">
+               <div className="float-left">
+
+                  <h3>Category List</h3> 
+
+                </div> 
+               <div className="float-right">
+
+               <Button variant="outline-primary">New </Button>
+
+                </div> 
+           </div>
+
 
             <Table striped bordered hover>
                 <thead>
@@ -36,7 +50,7 @@ const Category = () => {
                     <tr>
                     <td>{cat.id}</td>
                     <td>{cat.title}</td>
-                    <td>Edit | Delete</td>
+                    <td> <Button variant="outline-info">Edit</Button> <Button variant="outline-danger">Danger</Button> </td>
                    </tr>
                     
                     )}
