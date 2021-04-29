f5422b8f356ea137b94abe591b2fa4cba53eb0ad
Md. Altaf Hoshain <altaf@squaregroup.com>
2021-04-29 13:38:06 +0600

    Category fetched by using axios , hooks

--- src/components/category/Category.js
+++ src/components/category/Category.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import axios from 'axios';
 
 const Category = () => {
+
+    const [category, setCategory] = useState([])
+
+    useEffect(() => {
+        axios.get('https://jsonblob.com/api/979febba-a8b1-11eb-aa02-93cd55b52c59')
+            .then(res => {
+                const cats = res.data;
+                setCategory(cats);
+            })
+    },[])
+
     return (
         <div>
-           <p>Category</p> 
+
+            <h3>Category list</h3>
+
+            <ul>
+                {category.map(cat => <li>{cat.title}</li>)}
+            </ul>
         </div>
     );
 };
